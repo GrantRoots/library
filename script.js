d583557421848c8f34e82dfdfeb497a0e2fa4b97
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-08-28 21:24:46 -0700

    Make displayBooks
    display books is a function that displays all the books in the library

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  read = true
 }
 
 Book.prototype.info = function () {
-    return (this.title, this.author, this.pages, this.read)
+    return `This book is named ${this.title}, ${this.author} is the author, It has ${this.pages} pages, ${this.read}`
 }
 
 function addBookToLibrary(book) {
   // do stuff here
-  // add book in library
+  myLibrary.push(book)
 }
+
+const bookContainer = document.querySelector('#book-container')
+
+const last = myLibrary.length - 1
 
 function displayBooks () {
-    for (book in myLibrary) {
-        const card = document.createElement('div')
-        book.textContent = book.info()
-    }
+    bookContainer.innerHTML = '';
+    myLibrary.forEach(book => {
+        console.log(book.info())
+        const bookCard = document.createElement('div')
+        bookCard.textContent = book.info();
+        // add read button
+        const readButton = document.createElement('button')
+        readButton.textContent = 'Did you read this?'
+        readButton.setAttribute("id", "readButton");
+        //  add remove book button
+        const removeButton = document.createElement('button')
+        removeButton.textContent = 'Remove Book'
+        removeButton.setAttribute("id", "removeButton");
+        bookCard.appendChild(readButton)
+        bookCard.appendChild(removeButton)
+        bookContainer.appendChild(bookCard)
+    })
 }
-
-// new
 
 const showButton = document.getElementById("showDialog");
 const bookDialog = document.getElementById("bookDialog");
@@ -52,7 +67,13 @@ bookDialog.addEventListener("close", (e) => {
 // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
-  const newBook = new Book(title.value, author.value, pages.value)
+  let newBook = new Book(title.value, author.value, pages.value)
+  console.log(newBook.info())
   addBookToLibrary(newBook);
+  displayBooks();
   bookDialog.close(); // Have to send the select box value here.
 });
+
+function removeBook() {
+
+}
